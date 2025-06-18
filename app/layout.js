c42565ab52deaaa8a2933230245970de088455cf
samandeepbishnoi <samandeepbishnoi@gamil.com>
2025-06-18 12:54:46 +0530

    feat: initialize Next.js project with Tailwind CSS and Radix UI components
    - Added package.json with dependencies for Next.js, Radix UI, and Tailwind CSS.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG icons for various UI elements in the public directory.
    - Documented setup steps in steps.txt for project initialization.
    - Configured Tailwind CSS with custom theme and animations in tailwind.config.mjs.

---
+++ app/layout.js
@@ -0,0 +1,39 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AI Career Coach",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    
+      <html lang="en" suppressHydrationWarning>
+        
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            
+            <main className="min-h-screen">{children}</main>
+            
+
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with 💗 by Saman_Bishnoi</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section >
+      Hello 👋, welcome to my site!
+    </section>
+  );
+}
